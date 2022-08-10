8834c0cb476d040504c77b744d9436ec1291135a
ArenRitz <aren.dolbakian@gmail.com>
2022-08-10 17:35:58 -0400

    Add appointment form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React from 'react'
 import "components/Appointment/styles.scss";
-
+import Empty from "./Empty";
+import Header from './Header';
+import Show from "./Show";
 
 
 
@@ -10,6 +12,11 @@ export default function Appointment (props) {
 
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header  time={props.time} />
+      { props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>  }
+
+
+    </article>
   );
 }
