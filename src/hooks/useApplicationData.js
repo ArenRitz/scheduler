7d4d62497a2699262e865fad5cac2e110595d6e3
ArenRitz <aren.dolbakian@gmail.com>
2022-08-16 18:16:19 -0400

    Intergration testing with async/await

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -16,9 +16,9 @@ export default function useApplicationData() {
 
   useEffect(() => {    
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
@@ -81,14 +81,10 @@ function bookInterview(id, interview) {
   
   const url =`http://localhost:8001/api/appointments/${id}`;
 
-  let PutReq={
-    url,
-    method: 'PUT',
-    data: appointment
-  }
-  return axios(PutReq).then(() => {
-    setState({...state, appointments, days});
-  });
+
+  return axios.put(url, appointment).then(() => {
+    setState({...state, appointments,days});
+  })
 }
 
 
