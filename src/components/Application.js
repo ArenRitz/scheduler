e591b6399cf944db2cdd39de84d961f515ecea12
ArenRitz <aren.dolbakian@gmail.com>
2022-08-17 16:27:24 -0400

    Fix duplicate import

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,9 @@
 // Imports
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
